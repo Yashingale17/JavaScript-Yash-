6a455266261dcf02bbcb68d123e065013c819ad9
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-02 07:09:23 +0000

    operaters

---
+++ Javascript/operaters.js
@@ -0,0 +1,13 @@
+
+// == is a comparesion operates that compares two values and resultes in boolean falue ie true or false 
+
+console.log("2" == 2);
+
+// === tripple equal to is a strict comparesion operater in this this checks the data type as well 
+
+console.log("2" === 2); 
+// false   because it compares datatype as well so both are not same datatype one is string and other one is nunber
+
+console.log( 2 === 2 ); 
+// true  both the data is same and equal 
+
