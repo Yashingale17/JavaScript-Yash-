b398e5135bef67a00f327dbe6dc1b1b3bc821a55
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-06 07:38:15 +0000

    objects

--- Javascript/InterviewIMP.js
+++ Javascript/InterviewIMP.js
@@ -18,57 +18,63 @@
         Bigint
         Symbol */
 
-        const name = "Yash Ingale";
-        let age = 22;
-        let compare = (2 === 2);
-        let temp = null;
-        let und;
-        let bigNum = 13243563565645234563462345n;
-        let newSymbol = Symbol(" First Synbollll") 
+const name = "Yash Ingale";
+let age = 22;
+let compare = (2 === 2);
+let temp = null;
+let und;
+let bigNum = 13243563565645234563462345n;
+let newSymbol = Symbol(" First Synbollll")
 
-        console.log(typeof temp);
-        /* Typeof null is Object*/
-
-        
+console.log(typeof temp);
+/* Typeof null is Object*/
 
 
-      /*  Non-primitive :
-        this is exect apposit to the premitive 
-
-        Array 
-        Objects  
-        Functions 
-        */
-
-        const Arr = ["Yash" , 3 , "Js"];
-
-        let newObj = {
-            Name : "Yash Ingale",
-            Age : 22,
-        };
-
-        let myfunction = function() {
-            console.log("first function");
-        };
-
-        console.log(typeof Arr);
-
-          /* Typeof function is function and we call it as object function
-          
-           Typeof object is object 
-           Typeof Array is object */
-
-           console.log(Array.from({name: "Sahil"})); // in this we need to mention of what we need to make array either key or value other wise it will return empty array. 
 
 
-           // Question 
+/*  Non-primitive :
+  this is exect apposit to the premitive 
 
-          // Declare a symbole and used it in a object and print the output??
+  Array 
+  Objects  
+  Functions 
+  */
 
-          let mysym = Symbol("Key1"); // syntax 
+const Arr = ["Yash", 3, "Js"];
 
-          let obj1 = {
-            name : "Yash",
-            [mysym] : "key1" // propoer syntex to used it in object.
-          }
-          console.log(obj1[mysym]);
+let newObj = {
+    Name: "Yash Ingale",
+    Age: 22,
+};
+
+let myfunction = function () {
+    console.log("first function");
+};
+
+console.log(typeof Arr);
+
+/* Typeof function is function and we call it as object function
+ 
+ Typeof object is object 
+ Typeof Array is object */
+
+console.log(Array.from({ name: "Sahil" })); // in this we need to mention of what we need to make array either key or value other wise it will return empty array. 
+
+
+// Question 
+
+// Declare a symbole and used it in a object and print the output??
+
+const mySymbol = Symbol("myUniqueKey"); // Create a symbol
+
+let obj1 = {
+  name: "Yash",
+  [mySymbol] : "myUniqueKey"// Use the symbol as a property key
+};
+console.log(typeof obj1[mySymbol]); 
+
+const symbol1 = Symbol("mySymbol");
+const symbol2 = Symbol("anotherSymbol"); 
+
+console.log(typeof symbol1);  // Output: "symbol"
+console.log(typeof symbol2);
--- Javascript/Objects.js
+++ Javascript/Objects.js
@@ -1,10 +1,32 @@
+const sym1 = Symbol("Key1");
 
+// objects are in  the key and value pare e.g name : " Yash " key = name and value = "Yash"
 let obj1 = {
     name : "Yash",
+    "Full Name" : "Yash Ingale", // v can also declare or add a key value pare inside a object but v cannot access it using dot notation it can only accessed using bracket notation.
     age : 21,
     city : "Mumbai",
-    gmail: "sahil@gmail.com"
+    gmail: "sahil@gmail.com",
+    [sym1]: "key"
 };
 
 console.log(obj1.name); // Dot Notation to access values of objects 
 console.log(obj1["gmail"]); // bracket notation but it take string values 
+
+obj1.name = "Sahil"; // changes the value of thoe key. 
+console.log(obj1);
+
+//Object.freeze(obj1); // using freeze allows you to freeze the object that means you cannot makes change in the object.
+console.log(obj1);
+
+
+
+obj1.greeting = function() {
+    console.log("Hello there")
+}
+obj1.greeting2 = function() {
+    console.log(`Hello there ${this.name}`);
+}
+console.log(obj1.greeting());
+console.log(obj1.greeting2());
+
