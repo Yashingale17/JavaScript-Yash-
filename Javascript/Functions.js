97ebb7d0a27c58b53630c930f204ee4bc702e0fc
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-07 07:18:54 +0000

    array and object passing in function

--- Javascript/Functions.js
+++ Javascript/Functions.js
@@ -21,3 +21,28 @@ let newfun = function(Username) {
  }
 
  console.log(fun3(/*argument*/));
+
+
+ function cal (...num1) { // if we use more then one argument we can use spread operater or restt operater that will give you all the arguments in a array.
+    return num1
+ }
+ console.log(cal(100 , 200 , 300 , 400))
+
+ // passing or using arrays and objects in functions 
+
+ let obj= {
+    name : "Yash",
+    surname : "Ingale"
+ }
+ let objf = function (newobj) {
+    console.log(`my name is ${newobj.name} and surname is ${newobj.surname}`)
+ }
+ objf(obj);
+
+ // Array passing 
+ let arr = [ 1 , 2 , 3 , 4 , 5];
+ let arrf = function (newfun) {
+    return newfun["3"] 
+ }
+console.log(arrf(arr))
+
