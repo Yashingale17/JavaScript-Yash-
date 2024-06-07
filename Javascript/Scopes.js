65ca04899ac6f9ba1de2bebc102e52d5782f9c70
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-07 07:48:33 +0000

    nested function scope

--- Javascript/Scopes.js
+++ Javascript/Scopes.js
@@ -9,6 +9,23 @@
     var c = 5;
  }
 
- console.log(a) // both a and b will not print as it will show that a and b are not defined as they are inside the scope and let and const are block scope if declared inside the a scope then will not accessable outside of the scope 
- console.log(b)
+  // console.log(a) 
+  // both a and b will not print as it will show that a and b are not defined as they are inside the scope and let and const are block scope if declared inside the a scope then will not accessable outside of the scope 
+  // console.log(b)
  console.log(c) // var is a global scope 
+
+
+ // nested scope is a scope inside a scope 
+
+////// IMPPPPPP to just notice what the code does to under scope and globel scope does
+ function nest () {
+    const user = "Yash";
+
+    function nest2 () {
+        const userfullname = "Yash Ingale"
+        console.log(user);
+    }
+    // console.log(userfullname);
+    nest2()
+ }
+   nest()
