24ca6dee4c181b549234da17a719db7517eacd65
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-08 07:42:59 +0000

    Switch and if else

---
+++ Javascript/controlFlow/Switch.js
@@ -0,0 +1,25 @@
+let month = "April"
+
+switch (month) {
+    case "Jan":
+        console.log(1)
+        break;
+    case "fab":
+        console.log(2)
+        break;
+    case "March":
+        console.log(3)
+        break;
+    case "April":
+        console.log(4)
+        break;
+    case "May":
+        console.log(5)
+        break;
+    case "June":
+        console.log(6)
+        break;
+
+    default:
+        break;
+}
---
+++ Javascript/controlFlow/ifelse.js
@@ -0,0 +1,26 @@
+// if , else if and else 
+
+let name = "Yash Ingale";
+
+if (name ==="Yash Ingale") {
+    console.log("Username is right")
+} else {
+    console.log("User Name must be only in words")
+}
+
+
+let marks =107; 
+
+if ( marks >= 35 && marks <=39 ) {
+    console.log("Just pass")
+} else if ( marks > 40 && marks < 75) { 
+    console.log("Average result")
+}else if (marks > 75 && marks < 85) {
+    console.log("Good result")
+}else {
+    console.log("Excellent")
+}
+
+
+
+
