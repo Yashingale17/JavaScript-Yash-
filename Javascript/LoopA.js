78fd44d9fdd104311c05b849b7d46caf521c1caf
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-10 08:12:27 +0000

    for of loop

---
+++ Javascript/LoopA.js
@@ -0,0 +1,20 @@
+// for of 
+
+let myarr = [ 1 , 2 , 3 ,4 ,5,6];
+
+for (const myarr1 of myarr) {
+    console.log(myarr1);
+}
+
+
+let arr = [ "IronMan" , "SpiderMan" , "Thor"];
+
+for (const arr2 of arr) {
+    console.log(arr2)
+};
+
+let str = "Hello World"
+
+for (const str2 of str) {
+    console.log(str2)
+}
