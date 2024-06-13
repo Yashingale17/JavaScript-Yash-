efa4355796bcc3379f3470d963e88b6c8489e105
Yash <119027675+Yashingale17@users.noreply.github.com>
2024-06-13 06:12:10 +0000

    DOM

---
+++ Javascript/DOM/dom.js
@@ -0,0 +1,19 @@
+/* Accessing Element using 
+    getElementById **Id us uniques for every element
+    getElementByClassName
+    getElementByTag */
+    
+    const heading = document.getElementById("head1");
+    console.dir(heading);
+    console.log(heading);
+    
+    
+    const classss = document.getElementsByClassName("cleaas2");
+    console.dir(classss);
+    console.log(classss);
+    
+    
+    const tagName = document.getElementsByTagName("h1");
+    console.dir(tagName);
+    console.log(tagName);
+    
