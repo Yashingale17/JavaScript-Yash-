8a7a0fe11254a4e80ecb1ee2abbdc6fa06f65b8e
Yashingale17 <sahilingale329@gmail.com>
2024-06-30 13:27:52 +0530

    out

---
+++ Javascript/stacjheap.js
@@ -0,0 +1,23 @@
+// Stack (Primitive) Heap (Non-primitive)
+
+let name = "Yash Ingale"
+
+let changeName = name ;
+changeName = "Sahil Ingale";
+
+console.log(changeName);
+console.log(name);
+
+
+let obj1 = {
+    Name : "Yash Ingale",
+    age : 22,
+    email : "sahilingale@gmail.com"
+};
+
+let obj2 = obj1 ;
+
+obj2.Name = "Sahil Ingale";
+
+console.log(obj2.Name);
+console.log(obj1.Name);
